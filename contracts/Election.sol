e5942043977382c08992cf70e176d422c7648b4c
Kashan Haider <kashan.hz999@gmail.com>
2022-12-14 14:13:08 +0530

    Created Contract

---
+++ contracts/Election.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+  //Model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  //Store a candidate
+  mapping(uint => Candidate) public candidates;
+
+  //Fetch a candidate
+  //Store Candidate Count
+  uint public candidatesCount;
+
+  constructor() public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+  }
+
+
+} 
